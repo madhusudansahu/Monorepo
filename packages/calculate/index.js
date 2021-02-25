e36c84027d49b998d2ede5f91ec140c0d44f4a56
madhusudan sahu <madhusudan.sahu@sap.com>
2021-02-26 00:48:49 +0530

    updated

--- packages/calculate/index.js
+++ packages/calculate/index.js
@@ -1,5 +1,5 @@
 var addition = require('addition');
 var substration = require('substration');
 
-console.log("add"+addition.add(4,5));
+console.log("add(4+5)"+addition.add(4,5));
 console.log("substract(6-5)"+substration.substract(6,5));
